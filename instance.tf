fae4d7ccb164f016a0903ac87c28f86a814eb10e
mguler47 <mehmetgulerdevops@gmail.com>
2019-12-18 18:33:22 -0600

    last edit

--- instance.tf
+++ instance.tf
@@ -1,14 +1,10 @@
 resource "aws_instance" "web" { 
   ami           = "ami-00068cd7555f543d5" 
   instance_type = "t2.micro" 
-  associate_public_ip_address = "true"
+ associate_public_ip_address = "true"
   key_name = aws_key_pair.deployer.key_name
-  security_groups = ["allow_ssh"
 
-   
- 
-
-
+tags = { 
     Name = "HelloWorld" 
   } 
 } 
