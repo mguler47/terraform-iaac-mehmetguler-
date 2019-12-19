f130fdad67e3749590c8a740c7298bd7389d974c
mguler47 <mehmetgulerdevops@gmail.com>
2019-12-18 18:24:42 -0600

    edit

--- instance.tf
+++ instance.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "web" {
   key_name = aws_key_pair.deployer.key_name
   security_groups = ["allow_ssh"
 
-   user_data = file("userdata_file")
+   user_data = file"userdata_file"
  
 
 tags = { 
