8e48861785ccdf82d41c2838470c72dd3885c29d
mguler47 <mehmetgulerdevops@gmail.com>
2019-12-18 18:54:12 -0600

    edit

--- backed.tf
+++ backed.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mmy-tf-test-bucket-state-mehmet"
+    bucket = "my-tf-test-bucket-state-mehmet"
     key    = "infrastructure"
     region = "us-east-1"
   }
