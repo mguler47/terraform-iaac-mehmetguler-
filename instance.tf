058277841e3e9de157cf946b7c4e65430bcb7a5c
mguler47 <mehmetgulerdevops@gmail.com>
2019-12-21 12:29:59 -0600

    eddit type

--- instance.tf
+++ instance.tf
@@ -5,23 +5,19 @@ resource "aws_instance" "web" {
   associate_public_ip_address = var.associate_public_ip_address
   key_name = aws_key_pair.deployer.key_name
   security_groups = ["allow_ssh"]
-  
-
-
-provisioner "remote-exec" { 
-    connection { 
-      host        = "self.public_ip" 
-      type        = "ssh" 
-      user        = "var.user" 
-      private_key = "file(var.ssh_key_location)" 
-    } 
-    inline = [ 
-      "sudo yum install -y epel-release",
+  provisioner "remote-exec" { 
+  connection { 
+    host        = "self.public_ip" 
+    type        = "ssh" 
+    user        = "var.user"
+      private_key = "file(var.ssh_key_location)" 
+    } 
+inline = [ 
+  "sudo yum install -y epel-release",
       "sudo yuml install httpd -y"
       "systemctl start httpd " 
-    ] 
-
-  } 
+       ] 
+       } 
 
   lifecycle{
     prevent_destroy = false
