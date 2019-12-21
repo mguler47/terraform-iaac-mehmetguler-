ffc52df82df5f175941f986184fc7af6132475f8
mguler47 <mehmetgulerdevops@gmail.com>
2019-12-21 10:45:09 -0600

    remove imported

--- instance.tf
+++ instance.tf
@@ -1,16 +1,11 @@
 resource "aws_instance" "web" { 
-  count = 10 
   ami           = "ami-00068cd7555f543d5" 
   instance_type = "t2.micro" 
  associate_public_ip_address = "true"
   key_name = aws_key_pair.deployer.key_name
 
 tags = { 
-    Name = "HelloWorld${count.index +1} " 
+    Name = "HelloWorld " 
   } 
 } 
-resource "aws_instance" "imported" {
-   ami           = "ami-00068cd7555f543d5" 
-  key_name = aws_key_pair.deployer.key_name
-  security_group = ["aalows_ssh"]
-}
+
