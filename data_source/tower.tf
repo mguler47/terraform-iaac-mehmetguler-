d3da09bcfb27a98cefb288282a3cbb0046c687ce
mguler47 <mehmetgulerdevops@gmail.com>
2020-01-08 20:02:08 -0600

    edit

--- data_source/tower.tf
+++ data_source/tower.tf
@@ -1,53 +1,47 @@
 data "aws_ami" "centos" {
-    filter {
-        name = "root-device-type" 
-        values = ["ebs"] 
-} 
- filter { 
-     name = "name"
-     values = ["CentOS Linux 7 x86_64 HVM EBS *"] 
-} 
-    
-    most_recent = true
-    owners = ["679593333241"] 
+  filter {
+  name = "root-device-type"
+  values = ["ebs"]
 }
-output = "centos" {
-    value = "data.aws_ami.centos.id"
+filter {
+name   = "name"
+values = ["CentOS Linux 7 x86_64 HVM EBS *"]
+}
+  most_recent = true
+  owners = ["679593333241"]
 
 }
-resource "aws_key_pair" "towerkey" { 
-  key_name   = "towerkey" 
-  public_key = file("~/.ssh/id_rsa.pub") 
-} 
+output "centos" {
+  value = "${data.aws_ami.centos.id}"
+}
 
-
+resource "aws_key_pair" "towerkey" {
+key_name   = "towerkey"
+public_key = file("~/.ssh/id_rsa.pub")
+}
 resource "aws_instance" "tower" {
-  ami           = "data.aws_ami.centos.id"
+  ami           = "${data.aws_ami.centos.id}"
   instance_type = "t2.micro"
-  key_name =aws_key_pair.towerkey.key_name
+  key_name      = aws_key_pair.towerkey.key_name
+  provisioner "remote-exec" {
+       connection {
+       host = self.public_ip
+       type = "ssh"
+       user = "centos"
+       private_key = file("~/.ssh/id_rsa")
+       }
+       inline = ["sudo yum install -y epel-release",]
+     } 
 
-provisioner "remote-exec" {
-    connection {
-        host = "self.public_ip"
-        type = "ssh"
-        user = "centos"
-        private_key = file("~/.ssh/id_rsa")
-        }
-     inline = [
-         "sudo yum install -y epel-release",
-         ]
-         }
-         tags = {
-             Name = "HelloWorld"
-             }
-         tags {
-             Name = "HelloWorld"
-             }
+  tags = {
+    Name = "HelloWorld"
+  }
+}
 
 resource "aws_route53_record" "tower" { 
   zone_id = "Z27XYELYU6LHL5" 
   name    = "tower.example.com" 
   type    = "A" 
   ttl     = "300" 
-  records = ["aws_instance.tower.public_ip"] 
-} 
+  records = ["${aws_instance.tower.public_ip}"]
+}
