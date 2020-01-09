9992573c84f067005555a06dc533a55a74ec712c
mguler47 <mehmetgulerdevops@gmail.com>
2020-01-08 18:51:07 -0600

    add id

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -5,12 +5,20 @@ provider "aws" {
 
 
 data "aws_ami" "ubuntu" {
-    most_recent = true 
+    most_recent = true
     owners = ["099720109477"] 
 }
 
 
 
 output "ami" {
-    value = "${data.aws_ami.ubuntu.id}"
+    value = "${data.aws_ami.ubuntu.id}"
+}
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
