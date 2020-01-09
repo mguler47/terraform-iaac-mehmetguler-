827ee8b9103a07ffd1afe3136183f262bffad75d
mguler47 <mehmetgulerdevops@gmail.com>
2020-01-08 19:48:51 -0600

    edit text

--- data_source/tower.tf
+++ data_source/tower.tf
@@ -24,6 +24,8 @@ resource "aws_key_pair" "towerkey" {
   key_name   = "towerkey" 
   public_key = file("~/.ssh/id_rsa.pub") 
 } 
+
+
 resource "aws_instance" "web" {
   ami           = "data.aws_ami.centos.id"
   instance_type = "t2.micro"
@@ -31,9 +33,9 @@ resource "aws_instance" "web" {
 
 provisioner "remote-exec" {
     connection {
-        host        = "self.public_ip"
-        type        = "ssh"
-        user        = "centos"
+        host = "self.public_ip"
+        type = "ssh"
+        user = "centos"
         private_key = file("~/.ssh/id_rsa")
         }
      inline = [
