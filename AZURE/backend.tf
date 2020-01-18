4f580db014fca0c8fcec57caee804a3f9984ca89
mguler47 <mehmetgulerdevops@gmail.com>
2020-01-18 13:31:49 -0600

    edit backedn

--- AZURE/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "examplemehmet2"
-    storage_account_name = "abcd1234"
-    container_name       = "tfstate"
-    key                  = "prod.terraform.tfstate"
-    access_key           =""   
-  }
-}
